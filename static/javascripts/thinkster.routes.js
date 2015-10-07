8fd7d3d2e07b181b2faa3d54182ab54654075264
alex_ua <alex.u.a@mail.ru>
2015-10-07 19:26:44 +0300

    register, login, logout

---
+++ static/javascripts/thinkster.routes.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.routes')
+    .config(config);
+
+  config.$inject = ['$routeProvider'];
+
+  /**
+  * @name config
+  * @desc Define valid application routes
+  */
+  function config($routeProvider) {
+    $routeProvider.when('/register', {
+      controller: 'RegisterController', 
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).otherwise('/');
+
+    $routeProvider.when('/register', {
+      controller: 'RegisterController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/register.html'
+    }).when('/login', {
+      controller: 'LoginController',
+      controllerAs: 'vm',
+      templateUrl: '/static/templates/authentication/login.html'
+    }).otherwise('/');
+
+  }
+
+
+})();
